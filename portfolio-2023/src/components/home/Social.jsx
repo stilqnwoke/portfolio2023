f5e2e75108497b4d2426615a624e5cce2f679ad6
stilqnwoke <69080350+stilqnwoke@users.noreply.github.com>
2023-01-03 10:26:13 +0200

    main component created

---
+++ portfolio-2023/src/components/home/Social.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a href="" className="home__social-icon" target="_blank">
+        <i class="uil uil-facebook"></i>
+      </a>
+
+      <a href="" className="home__social-icon" target="_blank">
+        <i class="uil uil-linkedin"></i>
+      </a>
+
+      <a
+        href="https://github.com/"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-github-alt"></i>
+      </a>
+    </div>
+  );
+};
+
+export default Social;
